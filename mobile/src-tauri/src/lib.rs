883d4a1f793a11282fdd0746d3b86173115867e7
TO <th.theophyl.hoguet@gmail.com>
2026-03-15 12:53:23 +0100

    update bun.lock and plugin

--- mobile/src-tauri/src/lib.rs
+++ mobile/src-tauri/src/lib.rs
@@ -18,13 +18,13 @@ fn calculate_days_left(expiry_date: String, format: String) -> Result<i64, Strin
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let builder = tauri::Builder::default()
-        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, calculate_days_left]);
 
     #[cfg(any(target_os = "android", target_os = "ios"))]
     let builder = builder
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_barcode_scanner::init())
         .plugin(tauri_plugin_haptics::init());
 
