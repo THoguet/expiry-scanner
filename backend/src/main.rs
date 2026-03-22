89dde40c4cceca01086cff0a91f9a275d8b1846a
TO <th.theophyl.hoguet@gmail.com>
2026-03-22 21:46:07 +0100

    0.2.0

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -15,7 +15,7 @@ use tower_http::services::ServeDir;
 #[derive(Parser)]
 #[command(
     name = "Expiry Scanner CLI",
-    version = "0.2",
+    version = "0.2.0",
     about = "A CLI for managing the Expiry Scanner backend"
 )]
 pub struct Args {
