2fcf2a042791af1b8570219b02300fa173e28536
TO <th.theophyl.hoguet@gmail.com>
2026-03-28 13:12:15 +0100

    fix: suppress deprecation warning for light navigation bar visibility

--- mobile/src-tauri/gen/android/app/src/main/java/com/nessar/expiry_scanner/MainActivity.kt
+++ mobile/src-tauri/gen/android/app/src/main/java/com/nessar/expiry_scanner/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : TauriActivity() {
         WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
       )
     } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      @Suppress("DEPRECATION")
       window.decorView.systemUiVisibility =
         window.decorView.systemUiVisibility or android.view.View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
     }
